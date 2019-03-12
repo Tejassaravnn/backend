ddd9c02d3ad73a9d9009eca17a90dd9eae3d06dd
Tejas <tejassara1999@gmail.com>
2019-03-12 22:20:32 +0530

    mongo -w

--- models/eventmodel.js
+++ models/eventmodel.js
@@ -2,14 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EventSchema = new Schema({
-   /* event:[{ */
     game: {type: String, required: true, max: 100},
     location: {type: String, required: true, max: 100},
     time: {type: String, required: true},
-    role: {type: String, required: false, max: 100},
+    role: {type: String, required: true, max: 100},
     players_needed: {type: String, required: true},
-    count:{type: String, required: false}
-    /* }] */
+    count:{type: String, required: true}
 });
 
-module.exports = mongoose.model('realevent1', EventSchema);
+module.exports = mongoose.model('realevent2', EventSchema);
