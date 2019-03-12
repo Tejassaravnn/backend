31db8bae0d3af41c730f8b81fd685ae3548e50d7
Tejas <tejassara1999@gmail.com>
2019-03-12 17:07:27 +0530

    local mongo

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var app = express();
 
 var mongoose = require('mongoose');
 // var dev_db_url = 'mongodb://dbtest2:dbtest2@ds263520.mlab.com:63520/matchup-test';
-var url = 'mongodb://localhost:27017/events';
+// var url = 'mongodb://localhost:27017/events';
+var url = 'mongodb://192.168.43.29:27017/events';
 var mongoDB = url || process.env.MONGODB_URI;
 
 mongoose.connect(mongoDB,{ useNewUrlParser: true });
